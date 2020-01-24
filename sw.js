36955ad31d9d0f873ccb8fe400e4db24b5acf26c
jonathonwpowell <jonathonwpowell@gmail.com>
2020-01-24 17:05:56 -0500

    Add service worker file

---
+++ sw.js
@@ -0,0 +1,54 @@
+// https://developers.google.com/web/fundamentals/primers/service-workers/
+
+var CACHE_NAME = "bikeways-cache-v1";
+var urlsToCache = [
+    '/',
+    '/data/caryFacilities.geojson',
+    '/data/caryGreenways.geojson',
+    '/data/durham_shared.geojson',
+    '/data/osm_data.geojson'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
