6230c2983dc2f8682529377a563c856566eafe28
jonathonwpowell <jonathonwpowell@gmail.com>
2020-02-29 20:53:00 -0500

    Modify service worker to cache less, and lots of code cleanup

--- sw.js
+++ sw.js
@@ -32,27 +32,31 @@ self.addEventListener('fetch', function (event) {
                     return response;
                 }
 
-                return fetch(event.request).then(
-                    function (response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
+                return fetch(event.request)
 
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        var responseToCache = response.clone();
+                // TODO: FIGURE OUT A WAY TO MAKE CACHE EXPIRE
 
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
+                // return fetch(event.request).then(
+                //     function (response) {
+                //         // Check if we received a valid response
+                //         if (!response || response.status !== 200 || response.type !== 'basic') {
+                //             return response;
+                //         }
 
-                        return response;
-                    }
-                );
+                //         // IMPORTANT: Clone the response. A response is a stream
+                //         // and because we want the browser to consume the response
+                //         // as well as the cache consuming the response, we need
+                //         // to clone it so we have two streams.
+                //         var responseToCache = response.clone();
+
+                //         caches.open(CACHE_NAME)
+                //             .then(function (cache) {
+                //                 cache.put(event.request, responseToCache);
+                //             });
+
+                //         return response;
+                //     }
+                // );
             })
     );
 });
